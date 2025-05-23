098a34e4c5a2415cf367d23cd8053ab44afb1bb4
ryderys <youssefi.ashkan.ys@gmail.com>
2025-05-23 19:17:56 +0330

    token fix01

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -232,7 +232,7 @@ class UserAuthController {
         secure: process.env.NODE_ENV === "production",
         sameSite: "none",
         maxAge: 1000 * 60 * 60 * 24 * 7, //7 days
-        domain: process.env.NODE_ENV === "production" ? ".liara.run" : undefined
+        domain: process.env.NODE_ENV === "production" ? "clothing-store.liara.run" : undefined
       });
   }
 }
